25805583bbca90e7ce27df292693e184de791feb
Sam Bender <rednebmas@gmail.com>
2017-03-28 20:54:23 -0700

    class 1, basic tableviewcontroller

---
+++ ToDoList/TodoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoTableViewCell.swift
+//  ToDoList
+//
+//  Created by Sam Bender on 3/28/17.
+//  Copyright © 2017 Sam Bender. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
