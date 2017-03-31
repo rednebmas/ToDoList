0ee7244b334b41361246fbaad29471d7c88888f3
Sam Bender <rednebmas@gmail.com>
2017-03-30 17:51:38 -0700

    Inclass

--- ToDoList/TodoTableViewCell.swift
+++ ToDoList/TodoTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TodoTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var aLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- ToDoList/TodoTableViewController.swift
+++ ToDoList/TodoTableViewController.swift
@@ -26,26 +26,27 @@ class TodoTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return 1
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        let cell: TodoTableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoTableViewCell
 
         // Configure the cell...
+        // cell.textLabel?.text = "My first task"
+        cell.aLabel.text = "
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
